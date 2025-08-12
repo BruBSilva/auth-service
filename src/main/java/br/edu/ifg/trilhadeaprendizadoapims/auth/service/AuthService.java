5f4b4985020f55e7204236756d8c78f69742915d
Vitória Silva <silva.lopes.vitoria@gmail.com>
2025-08-12 12:45:07 -0300

    Correção

--- src/main/java/br/edu/ifg/trilhadeaprendizadoapims/auth/service/AuthService.java
+++ src/main/java/br/edu/ifg/trilhadeaprendizadoapims/auth/service/AuthService.java
@@ -18,7 +18,7 @@ public class AuthService {
     @Autowired
     private Util util;
 
-    public Auth cautenticar(AuthDTO authDTO) {
+    public Auth autenticar(AuthDTO authDTO) {
 
         //Cria os headers com o token de autenticação temporario para a requisição de /auth
         //já que antes de autenticar o usuário, não temos um token válido
